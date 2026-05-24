b33a8763f72dd8b9185ca07a87ae63d4e45209b6
satyabatihotel-code <satyabatihotel@gmail.com>
2026-05-24 21:22:39 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install",function(){
+self.skipWaiting();
+});
+
+self.addEventListener("activate",function(event){
+event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch",function(event){
+if(event.request.method !== "GET"){
+return;
+}
+
+event.respondWith(fetch(event.request));
+});
